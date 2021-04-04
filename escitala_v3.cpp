847e3a9c74459800ada8f83a3df09f45e62542cb
Renzo Leonardo Gallegos Vilca <renzo.gallegos@ucsp.edu.pe>
2021-04-04 14:09:43 -0500

    Added escitala_v3 containing Scytale class definition and implementation

---
+++ escitala_v3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "escitala_v3.h"
+
+int main() {
+    std::string msg;
+    short int col;
+    short int row;
+
+
+    short int opt;
+    std::cout << "Choose an option" << std::endl;
+    std::cout << std::right << std::setw(12)
+              << "Encode (1)" << std::endl;
+    std::cout << std::right << std::setw(12)
+              << "Decode (2)" << std::endl;
+    std::cout << "Option: ";
+    std::cin >> opt;
+    std::cin.ignore();
+
+    std::cout << "Columns: ";
+    std::cin >> col;
+    std::cin.ignore();
+
+    std::cout << "Rows: ";
+    std::cin >> row;
+    std::cin.ignore();
+
+    std::cout << "Message: ";
+    (void) getline(std::cin, msg);
+
+    Scytale<std::string> code_1(msg, col, row);
+
+    if (opt == 1) {
+        std::string code = code_1.encode();
+        std::cout << "\nEncoded message: " << code;
+    } else {
+        std::string code = code_1.decode();
+        std::cout << "\nDecoded message: " << code;
+    }
+    
+    return 0;
+}
---
+++ escitala_v3.h
@@ -0,0 +1,55 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <string>
+#include <iomanip>
+
+template <class T>
+class Scytale {
+
+private:
+    short int col;
+    short int row;
+    short int tmp = 0;
+    T msg;
+public:
+    Scytale(T message, short int columns, short int rows) {
+        msg = message;
+        col = columns;
+        row = rows;
+    }
+    T encode();
+    T decode();
+
+};
+
+template <typename T>
+T Scytale<T>::encode()
+{
+    T strCode;
+
+    for (int i{0}; i < col; ++i)
+    {
+        int k = i;
+        for (int j{0}; j < row; ++j)
+        {
+            if (k > msg.length())
+                strCode.push_back(' ');
+            else
+                strCode.push_back(msg[k]);
+            k = k + col;
+        }
+    }
+    return strCode;
+}
+
+template <typename T>
+T Scytale<T>::decode()
+{
+    tmp = col;
+    col = row;
+    row = tmp;
+    return encode();
+}
+
+#endif
