167538ad2d2c00c0003c786a5fe3a91fd667be9e
Renzo Leonardo Gallegos Vilca <renzo.gallegos@ucsp.edu.pe>
2021-04-15 13:47:28 -0500

    Added cesar_v2
    Implemented the decode function correctly and also added the positive remainder function

---
+++ Cesar/cesar_v2.cpp
@@ -0,0 +1,92 @@
+#include "cesar_v2.h"
+#include <string>
+
+template <typename T>
+Caesar<T>::Caesar(T message, int msgKey) {
+    msg = message;
+    key = msgKey;
+}
+
+template <typename T>
+int Caesar<T>::remainder (int a, int n) {
+	int r;
+	int q = a/n;
+	if (a > 0) {
+		r = a - q * n;
+	}
+	else {
+		r = a - (q - 1) * n;
+	}
+	return r;
+}
+
+template <typename T>
+T Caesar<T>::encode()
+{
+    std::string code;
+    short int length = msg.length();
+    // For keys greater than 25, we'll obtain the remainder between
+    // the key and 26. Explanation: "z" is located in the 25th place
+    // of the alpha member, so if we have 26 as a key, that would
+    // mean the real key is 0 because it will go around the lower
+    // case interval one time (that's 26 slots).
+    if (key > 25) {
+        key = remainder(key, 26);
+    }
+    for (int i{0} ; i < length ; i++) {
+        if (lowerAlpha.find(msg[i]) != std::string::npos) {
+            int pos = lowerAlpha.find(msg[i]);
+            int newPos = pos + key;
+            if (newPos > 25) {
+                newPos = remainder(newPos, 26);
+            }
+            code.push_back(lowerAlpha[newPos]);
+        } 
+        else if (upperAlpha.find(msg[i]) != std::string::npos) {
+            int pos = upperAlpha.find(msg[i]);
+            int newPos = pos + key;
+            if (newPos > 25) {
+                newPos = remainder(newPos, 26);
+            }
+            code.push_back(upperAlpha[newPos]);
+        }           
+        
+    }
+    return code;
+}
+
+template <typename T>
+T Caesar<T>::decode()
+{
+    key = -key;
+    std::string code;
+    short int length = msg.length();
+    // For keys greater than 25, we'll obtain the remainder between
+    // the key and 26. Explanation: "z" is located in the 25th place
+    // of the alpha member, so if we have 26 as a key, that would
+    // mean the real key is 0 because it will go around the lower
+    // case interval one time (that's 26 slots).
+    if (key > 25) {
+        key = remainder(key, 26);
+    }
+    for (int i{0} ; i < length ; i++) {
+        if (lowerAlpha.find(msg[i]) != std::string::npos) {
+            int pos = lowerAlpha.find(msg[i]);
+            int newPos = pos + key;
+            if (newPos < 0) {
+                newPos = remainder(newPos, 26);
+            }
+            code.push_back(lowerAlpha[newPos]);
+        } 
+        else if (upperAlpha.find(msg[i]) != std::string::npos) {
+            int pos = upperAlpha.find(msg[i]);
+            int newPos = pos + key;
+            if (newPos < 0) {
+                newPos = remainder(newPos, 26);
+            }
+            code.push_back(upperAlpha[newPos]);
+        }           
+        
+    }
+    return code;
+}
---
+++ Cesar/cesar_v2_main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip>
+#include "cesar_v2.cpp"
+
+/* Caesar cipher using lower case, upper case,
+   numbers and punctuation marks.
+
+   Cifrado César usando minúsculas, mayúsculas,
+   números y signos de puntuación.           */
+
+int main() {
+    std::string msg;
+    int key;
+    int opt;
+
+    std::cout << "Choose an option" << std::endl;
+    std::cout << std::right << std::setw(12)
+              << "Encode (1)" << std::endl;
+    std::cout << std::right << std::setw(12)
+              << "Decode (2)" << std::endl;
+    std::cout << "Option: ";
+    std::cin >> opt;
+    std::cin.ignore();
+
+    if ((opt != 1) && (opt != 2)) {
+        return 0;
+    }
+
+    std::cout << "Key: ";
+    std::cin >> key;
+    std::cin.ignore();
+
+    std::cout << "Message: ";
+    getline(std::cin, msg);
+
+    Caesar<std::string> csr(msg, key);
+
+    if (opt == 1) {
+        std::string code = csr.encode();
+        std::cout << "\nEncoded message: " << code;
+    }
+    if (opt == 2) {
+        std::string code = csr.decode();
+        std::cout << "\nDecoded message: " << code;
+    }
+
+    return 0;
+}
